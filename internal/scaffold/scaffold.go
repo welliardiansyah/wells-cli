6bc0d190fc43d42666bdae6a40a9cb7cd3f9ccd5
welli <doctercodes@gmail.com>
2025-10-30 10:44:55 +0700

    Update middleware handler response

--- internal/scaffold/scaffold.go
+++ internal/scaffold/scaffold.go
@@ -698,17 +698,21 @@ import (
 
 func Success(c *gin.Context, message string, data interface{}) {
 	c.JSON(http.StatusOK, gin.H{
-		"success": true,
-		"message": message,
-		"data":    data,
+		"success":     true,
+		"status_code": http.StatusOK,
+		"message":     message,
+		"data":        data,
+		"timestamp":   time.Now().Format(time.RFC3339),
 	})
 }
 
 func Error(c *gin.Context, status int, message string, err interface{}) {
 	c.JSON(status, gin.H{
-		"success": false,
-		"message": message,
-		"error":   err,
+		"success":     false,
+		"status_code": status,
+		"message":     message,
+		"error":       err,
+		"timestamp":   time.Now().Format(time.RFC3339),
 	})
 }
 `
